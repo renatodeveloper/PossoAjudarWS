4cef90e6cdf4ccdbfd059277ed4745a21b35996a
renato <renato_developer@hotmail.com>
2018-04-30 18:30:40 -0300

    Controller de Usuario.

--- src/br/com/possoajudarws/controler/UsuarioControler.java
+++ src/br/com/possoajudarws/controler/UsuarioControler.java
@@ -1,5 +1,13 @@
 package br.com.possoajudarws.controler;
 
-public class UsuarioControler {
+import br.com.possoajudarws.dao.UsuarioDao;
 
+
+public class UsuarioControler {
+	/*
+	 * Metodo responsavel por ser o controlador entre o resource e a camada dao
+	 */
+	public static String validaUsuario(String json){
+		return UsuarioDao.getInstance().validaUsuario(json);
+	}
 }
