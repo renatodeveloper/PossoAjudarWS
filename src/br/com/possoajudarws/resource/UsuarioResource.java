7857a650375b05a42b3e6064d49a85a07be0ec0b
renato <renato_developer@hotmail.com>
2018-05-02 18:42:33 -0300

    add recursos crud

--- src/br/com/possoajudarws/resource/UsuarioResource.java
+++ src/br/com/possoajudarws/resource/UsuarioResource.java
@@ -1,158 +1,135 @@
 package br.com.possoajudarws.resource;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.json.JSONObject;
 
 import br.com.possoajudarws.controler.UsuarioControler;
+import br.com.possoajudarws.model.ServiceResponse;
 import br.com.possoajudarws.model.Usuario;
+import br.com.possoajudarws.model.UsuarioList;
 
 /**
  * Classe responsavel por conter os metodos rest de acesso ao webservice
  */
-@Path("/usuario")
+@Path("/usuarios")
 public class UsuarioResource {
+	private static List<Usuario> usuarios = new ArrayList<Usuario>();
 	
-	/**
-	 *  Metodo que verifica se o servidor está no ar
-	 * @return String JSON
-	 */
+	
 	@GET
 	@Path("/conection")
-	@Produces("application/json")
-	//@Produces("aplication/xml")
-	public String testResourceConnection(){
-		JSONObject object = new JSONObject();
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)//@Produces("application/json")//@Produces("aplication/xml")
+	public ServiceResponse recuperarStatusConexao(){
+		ServiceResponse object = new ServiceResponse();
 		try{
-			object.put("status", 1000);
-			object.put("mensage", "The connection was a success");
+			object.code = "1000";
+			object.message = "The connection was a success";
 		}catch(Exception e){
 			e.getMessage().toString();
 		}	
-		return object.toString();
+		String retorno = object.toString();
+		return object;
 	}
 	
-	/**
-	 *  Metodo Responsável por fazer chamada ao controler
-	 * @return ArrayList Usuario
-	 */
+
 	@GET
 	@Path("/todos")
-	@Produces("application/json")
-	public ArrayList<Usuario> listarUsuarios(){
-		return UsuarioControler.listarTodos();
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public UsuarioList listarUsuarios(){
+		UsuarioList listRetorno = UsuarioControler.listarTodos();
+		return listRetorno;
+	}
+	
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{idUsuario}")
+	public Usuario getUsuario(@PathParam("idUsuario") int id) {
+		Usuario result = null;
+		  try{
+			  int searchUser  = id;
+			  if(searchUser>0){
+				  result = new Usuario();
+				  result.setDsNome("Nome sucesso");
+				  result.setDsLogin("Login sucesso");
+			  }
+			  if (result == null) {
+				   throw new WebApplicationException(404);
+				  }
+		  }catch(Exception e){
+			  e.getMessage().toString();
+		  }
+		  return result;
 	}
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	/**
-	 *  Metodo responsavel por fazer chamada ao controler
-	 * @return 
-	 * @return Boolean usuario
-	 */
+	@GET
+	@Path("/find/{name}")
+	@Produces(MediaType.APPLICATION_JSON)
+	 public List<Usuario> findByName(@PathParam("name") String name) {
+	  
+	  List<Usuario> result = new ArrayList<Usuario>();
+	  
+	  for (Usuario contact : usuarios) {
+	   if (contact.getDsNome() != null
+	     && contact.getDsNome().toLowerCase()
+	        .contains(name.toLowerCase())) {
+	    result.add(contact);
+	   }
+	  }
+	  
+	  return result;
+	 }
 	
 	
 	@POST
-	@Path("/login/stringImplicito")
-	@Produces("application/json")
-	//@Produces("aplication/xml")
-	public String getStatusLoginStringImplicito(@Context UriInfo info, @QueryParam ("paramJsonUser") String paramJsonUser){
-		String result = "NOK";
-		String json = info.getQueryParameters().getFirst("paramJsonUser");
-		try{
-			if(json != null && json.length()>0){
-				if(json.equals("Implicito")){
-					return "OK_Implicito";
-				}
-				return "NOK_Implicito";
-			}
-		}catch(Exception e){
-			e.getMessage().toString();
-		}
-	
-		return result;
+	@Path("/add")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Usuario add(Usuario user) {
+	  
+	  if (user.getDsNome() == null || user.getDsNome().trim().equals("")) {
+	   throw new WebApplicationException(Response.status(Response.Status.BAD_REQUEST)
+	     .entity("O nome do usuário é obrigatório").build());
+	  }
+	  Usuario usuario = new Usuario();
+	  usuario.setDsNome(user.getDsNome() + ".adicionado");
+	  usuario.setDsLogin(user.getDsLogin());
+	  usuario.setDsSenha(user.getDsSenha());
+	  
+	  return usuario;
 	}
 	
-	
-	@GET
-	@Path("/login/users/{username}")
-	@Produces("application/json")
-	//@Produces("aplication/xml")
-	public String getStatusLoginStringExplicito(@Context UriInfo info, @PathParam ("username") String paramJsonUser){
-		String result = "NOK";
-		String json = info.getPathParameters().getFirst("username");
-		try{
-			if(json != null && json.length()>0){
-				if(json.equals("Explicito")){
-					return "Explicito";
-				}
-				return "NOK_Explicito";
-			}
-		}catch(Exception e){
-			e.getMessage().toString();
-		}
-	
-		return result;
+	 
+	@PUT
+	@Path("/{idUsuario}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void update(@PathParam("idUsuario") int id, Usuario user) {
+	  usuarios.set(id - 1, user);
+	  user.setIdUsuario(usuarios.indexOf(user) + 1);
 	}
-	
-	
-	@GET
-	@Path("/login/users/json/{paramJsonUser}")
-	@Produces("application/json")
-	//@Produces("aplication/xml")
-	public String getStatusLoginJson(@Context UriInfo info, @PathParam ("paramJsonUser") String paramJsonUser){
-		String result = "NOK";
-		String json = info.getPathParameters().getFirst("paramJsonUser");
-		try{
-			if(json != null && json.length()>0){
-				return		UsuarioControler.validaUsuario(json);
-			}
-		}catch(Exception e){
-			e.getMessage().toString();
-		}
-	
-		return result;
+	  
+	@DELETE
+	@Path("/{idUsuario}")
+	public void delete(@PathParam("idUsuario") int id) {
+		 Usuario contact = usuarios.get(id - 1);
+		 usuarios.remove(contact);
 	}
-	
 	
 }
